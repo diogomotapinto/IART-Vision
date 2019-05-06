4d225b30a4df7a3be56501315d354ac67eabb4d0
Diogo Mota Pinto <diogomotapinto@gmail.com>
2019-05-06 14:37:52 +0100

    dataset added

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 from Greetings import Greetings
-import tensorflow as tf
+from Dog import Dog
 
 
-def main():
+def olaaf():
     name = input("Hey what is your name?   ")
     option = input("What greeting do you want:  ")
 
@@ -17,27 +17,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    olaaf()
 
-    # Tensor flow code
-    # Load and prepare the MNIST dataset. Convert the samples from integers to floating-point numbers:
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    # Build the tf.keras model by stacking layers. Select an optimizer and loss function used for training:
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Flatten(input_shape=(28, 28)),
-            tf.keras.layers.Dense(512, activation=tf.nn.relu),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(10, activation=tf.nn.softmax),
-        ]
-    )
-    model.compile(
-        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-    )
-
-    # Train and evaluate model:
-    model.fit(x_train, y_train, epochs=5)
-    model.evaluate(x_test, y_test)
